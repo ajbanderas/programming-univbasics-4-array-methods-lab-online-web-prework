0a49097cec9631179daf2d19cc01a2cf54ef48c6
Kevin Downs <kevin.downs04@gmail.com>
2019-09-29 18:51:23 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,17 +8,18 @@ def using_sort(array)
 end
 
 def using_reverse(array)
-
+  reversed_array = array.reverse
+  return reversed_array
 end
 
 def using_first(array)
-
+  array.first
 end
 
 def using_last(array)
-
+  array.last
 end
 
 def using_size(array)
-
+  array.size
 end
